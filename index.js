d9436305ed81240d449be41f1e49b35113e0b320
Anders Norrback Bornholm <anders@bornholm.se>
2016-12-19 16:21:15 +0100

    Correct parsing of times

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const server = http.createServer((req, res) => {
       queryParameters[pair.split('=')[0]] = pair.split('=')[1];
     });
 
-    console.log(queryParameters);
-
     return getSiteId(queryParameters.station)
       .then(siteId => {
         return getNextDepartures(siteId, parseInt(queryParameters.direction, 10));
@@ -27,14 +25,13 @@ const server = http.createServer((req, res) => {
           resultString = 'There are no trains leaving within the next half hour';
         } else if (nextTrains.length > 0) {
           let departureTime = moment(nextTrains[0].ExpectedDateTime);
-          resultString = `The next train for ${nextTrains[0].Destination} leaves at ${departureTime.format('HH:MM')} which is in ${nextTrains[0].DisplayTime.replace('min', 'minutes')}.`;
+          resultString = `The next train for ${nextTrains[0].Destination} leaves at ${departureTime.format('HH:mm')} which is in ${nextTrains[0].DisplayTime.replace('min', 'minutes')}.`;
 
           if (nextTrains.length > 1) {
             let nextDepartureTime = moment(nextTrains[1].ExpectedDateTime);
-            resultString += ` The one after that for ${nextTrains[1].Destination} leaves at ${nextDepartureTime.format('HH:MM')} which is in ${nextTrains[1].DisplayTime.replace('min', 'minutes')}.`;
+            resultString += ` The one after that for ${nextTrains[1].Destination} leaves at ${nextDepartureTime.format('HH:mm')} which is in ${nextTrains[1].DisplayTime.replace('min', 'minutes')}.`;
           }
         }
-        console.log('nextTrains', nextTrains);
         res.write(resultString);
         res.end();
       });
@@ -49,7 +46,6 @@ function getSiteId (siteName) {
   return request(`http://api.sl.se/api2/typeahead.json?key=73518a557fcf43e196111f14dd21a185&searchstring=${siteName}&stationsonly=true&maxresults=1`)
     .then(response => {
       response = JSON.parse(response);
-      console.log(response);
       return response.ResponseData[0].SiteId;
     });
 }
@@ -59,14 +55,10 @@ function getNextDepartures (siteId, direction) {
     .then(departures => {
       departures = JSON.parse(departures);
 
-      console.log('trains', departures.ResponseData.Trains);
 
       departures = departures.ResponseData.Trains.filter(departure => {
-        console.log('Checking', typeof departure.JourneyDirection, typeof direction);
         return departure.JourneyDirection === direction;
       });
-
-      console.log('filtered', departures);
 
       return departures;
     });
