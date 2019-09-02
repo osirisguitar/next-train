2a9dfc2d290a22b3213c540e75b153968e6e98e9
Anders Norrback Bornholm <anders@bornholm.se>
2019-09-02 16:26:10 +0200

    Fixed string parsing.

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const templates = {
     nextTrain: 'Tåget efter det mot {destination} avgår {departureTime}, vilket är {departureTimeRelative}.'
   },
   en: {
-    noTrains: 'There are no trains leaving within the next hour',
-    firstTrain: 'The next train headed for {destination} leaves at {departureTime}, which is {departureTimeRelative}',
-    nextTrain: 'The one after that headed for {destination} leaves at {departureTime}, which is {departureTimeRelative}'
+    noTrains: 'There are no trains leaving within the next hour.',
+    firstTrain: 'The next train headed for {destination} leaves at {departureTime}, which is {departureTimeRelative}.',
+    nextTrain: 'The one after that headed for {destination} leaves at {departureTime}, which is {departureTimeRelative}.'
   }
 }
 
@@ -27,6 +27,13 @@ const server = http.createServer((req, res) => {
       queryParameters[pair.split('=')[0]] = pair.split('=')[1];
     });
 
+    if (queryParameters.destinations) {
+      queryParameters.destinations = queryParameters.destinations.split(',')
+      queryParameters.destinations = queryParameters.destinations.map(destination => {
+        return decodeURI(destination)
+      })
+    }
+
     let currentTemplates = templates.en;
     if (queryParameters.lang === 'sv') {
       currentTemplates = templates.sv;
@@ -34,10 +41,11 @@ const server = http.createServer((req, res) => {
 
     return getSiteId(queryParameters.station)
       .then(siteId => {
-        return getNextDepartures(siteId, parseInt(queryParameters.direction, 10));
+        return getNextDepartures(siteId, parseInt(queryParameters.direction, 10), queryParameters.destinations);
       })
       .then(nextTrains => {
         let resultString;
+        let deviations = [];
 
         if (nextTrains.length === 0) {
           resultString = currentTemplates.noTrains;
@@ -45,15 +53,23 @@ const server = http.createServer((req, res) => {
           resultString = currentTemplates.firstTrain;
           resultString = createDepartureString(resultString, nextTrains[0], queryParameters.lang);
 
+          if (nextTrains[0].Deviations) {
+            deviations.push(nextTrain[0].Deviations)
+          }
+
           if (nextTrains.length > 1) {
             resultString += ' ' + createDepartureString(currentTemplates.nextTrain, nextTrains[1], queryParameters.lang);
+            if (nextTrains[1].Deviations) {
+              deviations.push(nextTrains[1].Deviations)
+            }
           }
         }
 
         let result = {
           speech: resultString,
           displayText: resultString,
-          source: 'NextTrain'
+          source: 'NextTrain',
+          deviations
         };
 
         res.setHeader('Content-Type', 'application/json');
@@ -72,15 +88,17 @@ function createDepartureString (templateString, trainDeparture, language) {
 
   let departureString = templateString.replace('{destination}', trainDeparture.Destination).replace('{departureTime}', departureTime.format('HH:mm'));
 
-  if (trainDeparture.DisplayTime.indexOf('1 min') !== -1) {
-    departureString = departureString.replace('{departureTimeRelative}', (language === 'sv' ? '1 minut' : '1 minute'));
+  if (trainDeparture.DisplayTime.match(/^1 min/)) {
+    departureString = departureString.replace('{departureTimeRelative}', (language === 'sv' ? 'om 1 minut' : ' in 1 minute'));
   } else if (trainDeparture.DisplayTime.indexOf('min') !== -1) {
     let inString = (language === 'sv' ? 'om' : 'in');
     let minuteString = (language === 'sv' ? 'minuter' : 'minutes');
     departureString = departureString.replace('{departureTimeRelative}', `${inString} ${trainDeparture.DisplayTime.replace('min', minuteString)}`);
   } else if (trainDeparture.DisplayTime.indexOf('Nu') !== -1) {
-    departureString = departureString.replace('{departureTimeRelative}' , (language === 'sv' ? 'nu' : 'now'));
-  } 
+    departureString = departureString.replace('{departureTimeRelative}', (language === 'sv' ? 'nu' : 'now'));
+  } else {
+    departureString = departureString.replace((language === 'sv' ? ', vilket är {departureTimeRelative}' : ', which is {departureTimeRelative}'), '')
+  }
 
   if (language !== 'sv') {
     departureString = departureString.replace('ä', 'eh').replace('Ä', 'Eh').replace('å', 'aw').replace('Å', 'Aw').replace('ö', 'eh').replace('Ö', 'eh');
@@ -97,7 +115,7 @@ function getSiteId (siteName) {
     });
 }
 
-function getNextDepartures (siteId, direction) {
+function getNextDepartures (siteId, direction, destinations) {
   return request(`http://api.sl.se/api2/realtimedeparturesV4.json?key=f6227d99bfb844b4be7093c06ff11858&siteid=${siteId}&timewindow=60&bus=false&tram=false&ship=false&metro=false`)
     .then(departures => {
       departures = JSON.parse(departures);
@@ -106,6 +124,14 @@ function getNextDepartures (siteId, direction) {
       departures = departures.ResponseData.Trains.filter(departure => {
         return departure.JourneyDirection === direction;
       });
+
+      if (destinations) {
+        departures = departures.filter(departure => {
+          return destinations.some(destination => {
+            return destination.toLowerCase() == departure.Destination.toLowerCase()
+          })
+        })
+      }
 
       return departures;
     });
